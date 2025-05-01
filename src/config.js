f084274c0e9358707cdb0b5138e328c397f0f7bf
hrizvi19 <19rizvH@gordons.school>
2025-05-01 16:18:07 +0100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDqDXxTl3VyrQV6FXpNlf5N-Niv87wrAH0",
+  authDomain: "tracker-557e3.firebaseapp.com",
+  projectId: "tracker-557e3",
+  storageBucket: "tracker-557e3.firebasestorage.app",
+  messagingSenderId: "598752148157",
+  appId: "1:598752148157:web:51a2a65cdf82f0676c55d4"
+};
+
+export const openAiKey = "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";
